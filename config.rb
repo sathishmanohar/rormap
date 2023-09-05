3ad2b80f0548e2be6ff8ee49e477eca56b5eccca
Sathish M <design.sathish@gmail.com>
2023-09-06 01:39:54 +0530

    Build with minified css/js, hash assets with relative path

--- config.rb
+++ config.rb
@@ -40,7 +40,9 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+  activate :asset_hash
+end
